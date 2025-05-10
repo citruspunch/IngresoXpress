0fa118f6d4ea867ce61c00c1b2bef5c2171e0dbf
andrestobar05 <andrewtobar05@gmail.com>
2025-05-10 14:55:48 -0600

    Refactor DepartmentsListView integration to include SidebarMenu for improved layout

--- app/features/departments/views/DepartmentsListView.tsx
+++ app/features/departments/views/DepartmentsListView.tsx
@@ -18,30 +18,28 @@ const DepartmentsListView = ({
   departments,
   ...props
 }: Props & ComponentProps<typeof Table>) => (
-  <>
-    <Table {...props}>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Código</TableHead>
-          <TableHead>Nombre</TableHead>
+  <Table {...props}>
+    <TableHeader>
+      <TableRow>
+        <TableHead>Código</TableHead>
+        <TableHead>Nombre</TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      {departments.map((department) => (
+        <TableRow key={department.id}>
+          <TableCell className="font-medium">{department.code}</TableCell>
+          <TableCell>{department.name}</TableCell>
         </TableRow>
-      </TableHeader>
-      <TableBody>
-        {departments.map((department) => (
-          <TableRow key={department.id}>
-            <TableCell className="font-medium">{department.code}</TableCell>
-            <TableCell>{department.name}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-      <TableFooter>
-        <TableRow>
-          <TableCell colSpan={3}>Total</TableCell>
-          <TableCell className="text-right">{departments.length}</TableCell>
-        </TableRow>
-      </TableFooter>
-    </Table>
-  </>
+      ))}
+    </TableBody>
+    <TableFooter>
+      <TableRow>
+        <TableCell colSpan={3}>Total</TableCell>
+        <TableCell className="text-right">{departments.length}</TableCell>
+      </TableRow>
+    </TableFooter>
+  </Table>
 )
 
 export default DepartmentsListView
--- app/routes/departments.tsx
+++ app/routes/departments.tsx
@@ -1,6 +1,7 @@
 import DepartmentsListView from '~/features/departments/views/DepartmentsListView'
 import { createClient } from '~/lib/supabase/server'
 import type { Route } from './+types/departments'
+import SidebarMenu from '~/components/sidebarmenu'
 
 export const loader = async ({ request }: Route.LoaderArgs) => {
   const { supabase } = createClient(request)
@@ -9,7 +10,7 @@ export const loader = async ({ request }: Route.LoaderArgs) => {
 }
 
 const Component = ({ loaderData }: Route.ComponentProps) => (
-  <DepartmentsListView departments={loaderData} />
+  <SidebarMenu component={<DepartmentsListView departments={loaderData} />} />
 )
 
 export default Component
