42bce78745ae35788eaf316d6741ab54e19d4751
andrestobar05 <andrewtobar05@gmail.com>
2025-05-24 14:05:55 -0600

    Refactor EmployeeReport types and enhance utility functions for time conversion and data grouping

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -31,21 +31,88 @@ export const fetchEmployeesWithDepartments = async (request: Request) => {
   return departmentEmployees
 }
 
-export type EmployeeReport =
-  | {
-      check_in: string
-      check_out: string
-      late_check_in: string
-      early_check_out: string
-      total_work_time: string
-      permission: Tables<'pass'>
-    }
-  | {
-      employee_id: string
-      employee_name: string
-      department: string
-      department_id: string
-      work_day: string
-    }
+export type employeeReportHeader = {
+  employee_id: string
+  employee_name: string
+  department: string
+  department_id: string
+  from: string
+  to: string
+  work_day: Tables<'work_day'>
+}
 
+export type EmployeeReport = {
+  check_in: string
+  check_out: string
+  late_check_in: string
+  early_check_out: string
+  total_work_time: string
+  permissions: Tables<'pass'>[]
+  observations: string
+}
 
+export const convertToTime = (
+  dateInTimestamptz: string,
+  dateInTimetz: string
+): number => {
+  const checkDate = new Date(dateInTimestamptz)
+  // Set the check-in time to the same date as the entry as it is in timetz format
+  const extractedDate = dateInTimetz.split('+')[0] // Remove time zone
+  const [entryHours, entryMinutes, entrySeconds] = extractedDate.split(':')
+  checkDate.setHours(
+    parseInt(entryHours),
+    parseInt(entryMinutes),
+    parseInt(entrySeconds)
+  )
+  return checkDate.getTime()
+}
+
+export const getTimeDifference = (
+  startTime: string,
+  endTime: string
+): string => {
+  const start = new Date(startTime)
+  const end = new Date(endTime)
+
+  const diffInMs = end.getTime() - start.getTime()
+  const diffInHours = Math.floor(diffInMs / (1000 * 60 * 60))
+  const diffInMinutes = Math.floor((diffInMs % (1000 * 60 * 60)) / (1000 * 60))
+
+  return `${diffInHours}h ${diffInMinutes}m`
+}
+
+export type EntryWithRelations = Tables<'entry'> & {
+  employee: Tables<'employee'> & {
+    department: Tables<'department'>
+    pass: Tables<'pass'>[]
+    work_day: Tables<'work_day'>
+  }
+}
+
+export const groupByDate = (
+  data: EntryWithRelations[]
+): Record<string, EntryWithRelations[]> => {
+  const grouped = data.reduce((acc, item) => {
+    const date = item.created_at.split('T')[0]
+    if (!acc[date]) {
+      acc[date] = []
+    }
+    acc[date].push(item)
+    return acc
+  }, {} as Record<string, EntryWithRelations[]>)
+  console.log('Grouped data:', grouped)
+  return grouped
+}
+
+export const getWorkingDaysInSpanish = (days: string[]): string[] => {
+  const daysInSpanish: Record<string, string> = {
+    monday: 'Lunes',
+    tuesday: 'Martes',
+    wednesday: 'Miércoles',
+    thursday: 'Jueves',
+    friday: 'Viernes',
+    saturday: 'Sábado',
+    sunday: 'Domingo',
+  }
+  return days.map((day) => daysInSpanish[day])
+}
