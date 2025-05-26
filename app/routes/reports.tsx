a92e858a40b63d7e9f65d2ee6665f4366c7c1a78
andrestobar05 <andrewtobar05@gmail.com>
2025-05-26 09:38:16 -0600

    Refactor AttendancePermissionView and AttendanceReportInput components; adjust spacing and enhance layout with Card component for better UI presentation

--- app/routes/reports.tsx
+++ app/routes/reports.tsx
@@ -2,6 +2,7 @@ import SidebarMenu from '~/components/SidebarMenu'
 import type { Route } from './+types/reports'
 import AttendanceReportInput from '~/features/reports/views/AttendanceReportInput'
 import { fetchEmployeesWithDepartments } from '~/lib/utils'
+import { Separator } from '~/components/ui/separator'
 
 export const loader = async ({ request }: Route.LoaderArgs) => {
   return fetchEmployeesWithDepartments(request)
@@ -9,6 +10,8 @@ export const loader = async ({ request }: Route.LoaderArgs) => {
 
 const Component = ({ loaderData }: Route.ComponentProps) => (
   <SidebarMenu>
+    <h2 className="font-bold text-3xl mb-1">Reportes de Asistencia</h2>
+    <Separator className="mb-1" />
     <AttendanceReportInput employees={loaderData!} />
   </SidebarMenu>
 )
