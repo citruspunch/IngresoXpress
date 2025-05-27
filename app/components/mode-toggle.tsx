cea14a28ef90f15a0eced0cf4b3d1cb659e366bf
andrestobar05 <andrewtobar05@gmail.com>
2025-05-27 11:47:05 -0600

    Update ModeToggle component size to large and enhance icon styling; add new Entries route with basic structure

--- app/components/mode-toggle.tsx
+++ app/components/mode-toggle.tsx
@@ -9,11 +9,12 @@ const ModeToggle = () => {
   return (
     <Button 
       variant="ghost"
-      size="sm"
+      size="lg"
       onClick={() => setTheme((prev) => (prev === Theme.LIGHT ? Theme.DARK : Theme.LIGHT))}
+      className='has-[>svg]:py-0 has-[>svg]:px-1.5'
     >
-      <Sun2 className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <Sun2 className="rotate-0 w-[1.5rem] h-[1.5rem] scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute w-[1.5rem] h-[1.5rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
     </Button>
   )
 }
---
+++ app/routes/entries.tsx
@@ -0,0 +1,9 @@
+const Component = () => {
+  return (
+    <div>
+      <h1>Entries</h1>
+      <p>This is the entries page.</p>
+    </div>
+  )
+}
+export default Component
